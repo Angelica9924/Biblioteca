847f90400389fcc427582bc7415b1465f491377a
Angelica9924 <149621614+Angelica9924@users.noreply.github.com>
2024-03-24 21:05:14 -0500

    Add files via upload

---
+++ Controllers/HistorialController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+    namespace Mvc.Controllers{
+        public class HistorialController : Controller{
+            public IActionResult Index(){
+                return View();
+            }
+            public IActionResult Show(){
+                return View();
+            }
+            public IActionResult Perdidos(){
+                return View();
+            }
+            public IActionResult Popularidad(){
+                return View();
+            }
+            public IActionResult GeneroLector(){
+                return View();
+            }
+        }
+    }
